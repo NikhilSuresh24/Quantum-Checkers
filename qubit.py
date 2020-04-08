b990badad367871b4fb40d743b0eca699d9186ab
Max Gotts <maxgotts@nus10713-usloaner19-004.nueva.local>
2020-04-08 13:08:09 -0700

    screenshot and updates to qubit

--- qubit.py
+++ qubit.py
@@ -1,23 +1,14 @@
 
 class Qubit:
-    def __init__(self, init_state, init_position, possible_positions):
-        super().__init__()
-        self.state = init_state
-        self.position = init_position
-        self.possible_positions = possible_positions
+    def __init__(self, init_wavefunction=-1, rows=5, columns=5):
+        self.wavefunction = init_wavefunction
+        if self.wavefunction==-1:
+            self.wavefunction = [[0 for i in range(rows)] for j in range(columns)]
+            self.wavefunction[3][3] = 1
 
-    def get_probability(self, x_position, y_position, delta_time): #TODO:implement
-        '''Get the probability of being at (x,y) after delta_time'''
-        pass
+    def at(self, x, y): # Calculate the probability amplitude at (x,y)
+        return self.wavefunction[x-3][y-3]
 
     def get_next_location(self, delta_time): #TODO:implement
         '''Use get_probability to get probability of being at any locations, uses those probabilities to get the next position, then finally updates position'''
-        pass
-
-    def update_state(self, new_state):
-        '''set new state depending on what simulation does'''
-        self.state = new_state
-
-    def get_spin(self): #TODO: implement
-        '''gets spin of qubit and adjusts the state accordingly'''
         pass
