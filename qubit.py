58bb3833e54f1c70075abd653d27bf09f4804c70
Max Gotts <maxgotts@nus10713-usloaner19-004.nueva.local>
2020-04-08 18:44:46 -0700

    readme for qubit.py

--- qubit.py
+++ qubit.py
@@ -2,11 +2,21 @@ import numpy as np
 
 class Qubit:
     def __init__(self, init_wavefunction=-1, rows=5, cols=5, maxtime=1, N=10):
+        '''
+        Give an initial wavefunction state, the size of the 2D field, the extent of the paremetising t-variable, and the level of discretization
+        It then runs ::propogate_wavefunction maxtime*N+1 times (cf. ::generate_wavefunction) to create a 3D array of the wavefunction (.psi) in three variables: k, x, y
+        With x:0->rows-1 and y:0->cols-1 and k:0->maxtime*N+1
+        But ::Psi corrects for this and translates .psi to a reasonable format:
+        ::Psi takes three variables: t, x, y. t is "real time" (i.e. discretisation ignored) and x,y are defined so that they are centered at 0 (e.g. -2, -1, 0, 1, 2)
+        ::Prob calculates the probability based on the probability amplitude (i.e. |Psi(t,x,y)|^2)
+        '''
+
         self.wavefunction = init_wavefunction
         self.rows = rows # first index
         self.cols = cols # second index
         self.xcenter = int(np.ceil(self.rows/2))
         self.ycenter = int(np.ceil(self.cols/2))
+        if round(rows/2)==rows/2 or round(cols/2)==cols/2: print("Even dimensions will make things look strange, proceeding anyway")
 
         self.k = 0
         self.N = N
